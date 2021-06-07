3411009af40c7acaa2bba011844fdbec5c7d68fd
niitsumario <xxx.ballet121.xxx@gmail.com>
2021-06-07 09:24:36 +0000

    [Add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
   devise_for :users
+  get '/search', to: 'searchs#search'
   resources :users, only: [:index, :show, :edit, :update] do
     member do
      get :following, :followers
